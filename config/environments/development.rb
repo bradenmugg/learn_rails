87410ca5fb386031381945376d81f49d39104c5f
bradenmugg <braden.mugg@gmail.com>
2015-07-07 03:34:55 +0000

    Add mailing list

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   port: 587,
   domain: Rails.application.secrets.domain_name, authentication: "plain",
   enable_starttls_auto: true,
-  user_name: Rails.application.secrets.email_provider_username, 
+  user_name: "braden.mugg@gmail.com", 
   password: Rails.application.secrets.email_provider_password
 }
  # ActionMailer Config
